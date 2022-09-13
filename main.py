0be761580990f8783b57830ea618fa5949d405c6
NicCasperson11 <817687@mystma.org>
2022-09-13 09:41:00 -0500

    3 functions

---
+++ main.py
@@ -0,0 +1,27 @@
+def addTwoNumbers():
+    x = 82
+    y = 125
+    print (x + y)
+
+addTwoNumbers()
+
+def printName():
+    print("Nic")
+
+printName()
+
+def printTopFive():
+    print("Top 5 The Mario game")
+    print("5. mario on the ps4")
+    print("4. MARIO goes to the store to buy a gallon of milk")
+    print("3. Mario minestrone THE GAME")
+    print("2. Wario Land 4 on the Game Boy Advance")
+    print("1. Super marrow")
+
+printTopFive()
+
+def scaryFunction():
+    print("Minions " * 666)
+
+scaryFunction()
+
